a5c5965d3fd80e3e224fffaa3777446b241076e8
Roland Tritsch <roland@tritsch.org>
2019-07-13 12:39:44 +0100

    Haskell: Tail recursive. Rename fib' to fib2 (for consistency).

--- haskell/src/Fib.hs
+++ haskell/src/Fib.hs
@@ -5,7 +5,7 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
-fib' :: Integer -> Integer
-fib' n = go n (0, 1) where
+fib2 :: Integer -> Integer
+fib2 n = go n (0, 1) where
   go 0 (a, _) = a
   go n (a, b) = go (n - 1) (b, a + b)
