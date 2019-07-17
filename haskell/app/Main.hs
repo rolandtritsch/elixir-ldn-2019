d7e46149a97f8652366d44506ddf898344177470
Roland Tritsch <roland@tritsch.org>
2019-07-17 12:15:39 +0100

    Haskell: Add microbenchmark. Simplify.

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -10,15 +10,9 @@ import Fib
 main :: IO ()
 main = do
   args <- getArgs
-  case args of
-    [n, n1, n2] -> run (map read args)
-    _ -> printf "Usage: run n n1 n2"
-
-run :: [Int] -> IO ()
-run args = do
-  (time, result) <- timeItT $ evaluate $ fib (args !! 0)
+  (time, result) <- timeItT $ evaluate $ fib (read $ args !! 0)
   printf "fib (%f): %d\n" time result
-  (time2, result2) <- timeItT $ evaluate $ fib2 (args !! 1)
+  (time2, result2) <- timeItT $ evaluate $ fib2 (read $ args !! 1)
   printf "fib2 (%f): %d\n" time2 result2
-  (time3, result3) <- timeItT $ evaluate $ fib3 (args !! 2)
+  (time3, result3) <- timeItT $ evaluate $ fib3 (read $ args !! 2)
   printf "fib3 (%f): %d\n" time3 result3
