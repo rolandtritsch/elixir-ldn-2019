3293803566a83a13c6419f40580ae61364866321
Roland Tritsch <roland@tritsch.org>
2019-07-17 13:06:42 +0100

    Scala: Add microbenchmark. Change time to sec (for consistency).

--- scala/README.md
+++ scala/README.md
@@ -4,3 +4,4 @@ To make this work, you need to run ...
 
 * `sbt test`
 * `sbt "run 42 10000 10000"`
+* Note: To force this to run on one CPU only you can use `taskset --cpu-list 1 sbt "run 42 10000 10000"` (on Ubuntu)
--- scala/src/main/scala/Util.scala
+++ scala/src/main/scala/Util.scala
@@ -1,8 +1,8 @@
 object Util {
-  def timeIt[R](block: => R): (Long, R) = {
+  def timeIt[R](block: => R): (Double, R) = {
     val startTime = System.currentTimeMillis()
     val result = block
     val endTime = System.currentTimeMillis()
-    (endTime - startTime, result)
+    ((endTime - startTime)/1000.0, result)
   }
 }
