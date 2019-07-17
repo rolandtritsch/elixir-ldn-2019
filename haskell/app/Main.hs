7522162bea065917545b464c5e580c840848d886
Roland Tritsch <roland@tritsch.org>
2019-07-17 11:47:53 +0100

    Haskell: Add microbenchmark.

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,24 @@
 module Main where
 
 import System.Environment
+import System.TimeIt (timeItT)
+import Text.Printf (printf)
+import Control.Exception (evaluate)
 
 import Fib
 
 main :: IO ()
 main = do
   args <- getArgs
-  print $ fib3 $ read $ head args
+  case args of
+    [n, n1, n2] -> run (map read args)
+    _ -> printf "Usage: run n n1 n2"
+
+run :: [Int] -> IO ()
+run args = do
+  (time, result) <- timeItT $ evaluate $ fib (args !! 0)
+  printf "fib (%f): %d\n" time result
+  (time2, result2) <- timeItT $ evaluate $ fib2 (args !! 1)
+  printf "fib2 (%f): %d\n" time2 result2
+  (time3, result3) <- timeItT $ evaluate $ fib3 (args !! 2)
+  printf "fib3 (%f): %d\n" time3 result3
