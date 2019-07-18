13e4aa6178e71bb14836273dc6b95f53ffdd2214
Roland Tritsch <roland@tritsch.org>
2019-07-18 14:09:11 +0100

    Elixir: Use better function names.

--- elixir/lib/fib.ex
+++ elixir/lib/fib.ex
@@ -5,9 +5,9 @@ defmodule Fib do
   def fib(1), do: 1
   def fib(n), do: fib(n - 1) + fib(n - 2)
 
-  def fib2(n), do: go2(n, {0, 1})
-  defp go2(0, {a, _}), do: a
-  defp go2(n, {a, b}), do: go2(n - 1, {b, a + b})
+  def fib2(n), do: fib2p(n, {0, 1})
+  defp fib2p(0, {a, _}), do: a
+  defp fib2p(n, {a, b}), do: fib2p(n - 1, {b, a + b})
 
   def fib3(n), do: fibs() |> Enum.at(n)
   defp fibs(), do: Stream.unfold({0, 1}, fn {a, b} -> {a, {b, a + b}} end)
